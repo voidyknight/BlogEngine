662b6a3836b07064ee02a6bb27c33b74508134a4
kig12345 <kig-12345@hotmail.com>
2014-10-20 11:48:53 -0400

    starting pull.py

---
+++ pull.py
@@ -0,0 +1,14 @@
+
+
+def pullBlog(blogName){
+    bName = open('insert file here', 'r')
+    bName = bName.readlines()
+    int counter = 0;
+    while (counter < bName.len()):
+        if (bName[counter] ==  blogName):
+
+
+        counter = bName.len()
+        
+    
+}
