e8304ab10b814eb3bd7572981d32fac0d5e251a2
mchaiken <mirandachaiken@gmail.com>
2014-10-20 16:53:05 -0400

    get blog posts

--- pull.py
+++ pull.py
@@ -14,9 +14,16 @@ def pullBlog(blogName):
 
         counter = bName.len()
     '''
+    query="select post from blogs posts where name == blog";
+    results = c.execute(q)
+    
 def addPost(blog, post):
     q ="INSERT TO posts VALUES("+blog+","+post+");"
-    result = c.execute(q)
+    results = c.execute(q)
+    posts=[]
+    for post in results:
+        posts.append(post[0])
+    return posts
     
 def getBlogs():
     q ="""
