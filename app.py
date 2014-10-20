9fb1f66a9b2bc5f334ebcdf28dc59fda09726c8c
mchaiken <mirandachaiken@gmail.com>
2014-10-19 22:13:43 -0400

    Set up CSS stuff and a home page?

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-app.route("/")
+@app.route("/")
 def main():
   return render_template("main.html")
   
-app.route("/<postID>")
-def post_page(postID = None):
+#@app.route("/<postID>")
+#def post_page(postID = None):
   #return render_template("post.html", name = post_name, content = post_content)
   #from postID, we access the database to extract post_name and post_content
+if __name__=="__main__":
+    app.debug=True
+    app.run(host="0.0.0.0",port=5000)
+    app.run()
